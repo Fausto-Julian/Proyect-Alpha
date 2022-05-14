e3090466c84cf4aed6b7ff22d28ff8dacea59450
Julian Fausto <julianfausto09@gmail.com>
2022-05-14 01:14:09 -0300

    Revert "update"
    This reverts commit 10ba051ac4379b66ae7f6471ecec4f7b4620ba11.

---
+++ Proyect Alpha/Assets/_main/Scripts/PoolGeneric.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolGeneric
+{
+    private List<GameObject> availables = new List<GameObject>();
+    private List<GameObject> inUse = new List<GameObject>();
+
+    public int AvailablesCount => availables.Count;
+
+    public GameObject GetorCreate()
+    {
+        if (availables.Count > 0)
+        {
+            var obj = availables[0];
+            availables.RemoveAt(0);
+            inUse.Add(obj);
+            obj.SetActive(true);
+            return obj;
+        }
+        return null;
+    }
+
+    public void InUseToAvailables(GameObject poolEntry)
+    {
+        poolEntry.SetActive(false);
+        inUse.Remove(poolEntry);
+        availables.Add(poolEntry);
+    }
+}
