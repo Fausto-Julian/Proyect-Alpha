f6c58fb2968e81d76eb06c76b8cd4aa8f712fc93
NFPerera <niko22602@gmail.com>
2022-07-14 15:19:52 -0300

    MAESTRO DEL TESTING

--- Proyect Alpha/Assets/_main/Scripts/CommandCheat.cs
+++ Proyect Alpha/Assets/_main/Scripts/CommandCheat.cs
@@ -5,7 +5,7 @@ public class CommandCheat : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F1))
+        if (Input.GetKeyDown(KeyCode.J))
         {
             if (SceneManager.GetActiveScene().buildIndex > 1)
             {
@@ -13,14 +13,14 @@ public class CommandCheat : MonoBehaviour
             }
         }
         
-        if (Input.GetKeyDown(KeyCode.F2))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         
-        if (Input.GetKeyDown(KeyCode.F3))
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            if (SceneManager.GetActiveScene().buildIndex < 7)
+            if (SceneManager.GetActiveScene().buildIndex < 11)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
--- Proyect Alpha/Assets/_main/Scripts/Enemys/SpikeHeadController.cs
+++ Proyect Alpha/Assets/_main/Scripts/Enemys/SpikeHeadController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SpikeHeadController : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
         {
